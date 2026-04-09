67cdad35b3c96e54b72a46183e385fe212ac3327
jaykantyadavswe <jaykant.yadav.cse@gmail.com>
2026-04-10 01:44:31 +0530

    Solve errors on New Page

--- app.js
+++ app.js
@@ -51,7 +51,6 @@ app.get("/listings/:id", async(req, res) => {
 
 // Create Route
 app.post("/listings", async(req, res) => {
-    // let {title, description, image, price, country, location} = req.body;
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
