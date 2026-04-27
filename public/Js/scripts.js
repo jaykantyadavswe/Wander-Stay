cad08a09b0aa1924e3c4dfb29a64ade9b8583821
jaykantyadavswe <jaykant.yadav.cse@gmail.com>
2026-04-28 02:34:02 +0530

    responsive added

--- public/Js/scripts.js
+++ public/Js/scripts.js
@@ -19,3 +19,23 @@ if (`<%- currUser %>`) {
 } else {
   console.log("Guest user");
 }
+
+// Filtor Scroller
+const scroller = document.getElementById('filterScroller');
+    let pos = 0, paused = false;
+
+    function autoScroll() {
+        if (!paused) {
+            const max = scroller.scrollWidth - scroller.clientWidth;
+            pos += 0.6;
+            if (pos >= max) pos = 0;
+            scroller.scrollLeft = pos;
+        }
+        requestAnimationFrame(autoScroll);
+    }
+    autoScroll();
+
+    scroller.addEventListener('mouseenter', () => paused = true);
+    scroller.addEventListener('mouseleave', () => paused = false);
+    scroller.addEventListener('touchstart', () => paused = true);
+    scroller.addEventListener('touchend', () => setTimeout(() => paused = false, 2000));
